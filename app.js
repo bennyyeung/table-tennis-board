ea7f5fcaa6df3e2ea1756ad969ad047be111f4a8
Benny Yeung <benny.tf@gmail.com>
2024-04-30 15:57:11 +0100

    Add Winner & Loser Classes with inline styles

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ p1Button.addEventListener('click', function () {
     if(!isGameOver) {
         p1Score+= 1;
         if(p1Score  === winningScore) {
-            isGameOver = true;                           
+            isGameOver = true;
+            p1Display.classList.add('winner')
+            p2Display.classList.add('loser')
         }
         p1Display.textContent = p1Score; 
     }      
@@ -23,20 +25,27 @@ p2Button.addEventListener('click', function () {
     if(!isGameOver) {
         p2Score+= 1;
         if(p2Score  === winningScore) {
-            isGameOver = true;                           
+            isGameOver = true;    
+            p2Display.classList.add('winner')
+            p1Display.classList.add('loser')                       
         }
         p2Display.textContent = p2Score; 
     }      
 })
 
 winningScoreSelect.addEventListener('change', function () {
-    alert("CHANGE!")    
+    winningScore = parseInt(this.value);
+    reset();
 })
 
-resetButton.addEventListener('click', function () {
+resetButton.addEventListener('click', reset)
+
+function reset () {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
-    p1Display.textContent = 0
-    p2Display.textContent = 0
-})
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
+}
